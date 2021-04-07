5ed148f757af17d8ab5ee2908991a067c3969270
Apple <apple@Apples-MacBook-Pro.local>
2021-04-07 22:08:27 +0500

    Added Validations to Sign Up Password Field & Added Privacy policy Text

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	validates :full_name, presence: true
+	validates :password, length:{minimum: 8}, format:{with: /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
