93af8e74701b9136b24ac0e39296b819dc96e67f
Apple <apple@Apples-MacBook-Pro.local>
2021-04-09 16:04:33 +0500

    added login through email or full name on login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
-	  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password, :password_confirmation)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :current_password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:full_name, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:full_name, :email, :password, :current_password, :password_confirmation)}
+  end
+
+  def configure_permitted_parameters
+    added_attrs = [:full_name, :email, :password, :password_confirmation, :remember_me]
+    #devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,24 @@
 class User < ApplicationRecord
   validates :full_name, presence: true
   validates :password, length:{minimum: 8}, format:{with: /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/}
+  validates :full_name, presence: true, uniqueness: { case_sensitive: false }
   # Include default devise modules. Others available are:
   # :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :lockable, :confirmable
+
+  attr_writer :login
+
+  def login
+    @login || self.full_name || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(full_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:full_name) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
 end
