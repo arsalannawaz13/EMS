b7705efaceaf771cf07f07bd6a6bfd5f11314c7c
Apple <apple@Apples-MacBook-Pro.local>
2021-04-09 18:01:33 +0500

    added admin user in db

--- app/javascript/custom/script.js
+++
@@ -1,9 +0,0 @@
-// $(document).on('ready turbolinks:load', () => {
-// 	toggleSignUp();
-// })
-
-// function toggleSignUp(){
-// 	$('#new-ems').on('click', function(){
-// 		$('#sign-up-form').removeClass('d-none');
-// 	})
-// }
