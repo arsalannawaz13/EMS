f659c36b8a7bcd2784787f3a4bea067da8de3e71
Apple <apple@Apples-MacBook-Pro.local>
2021-04-12 11:42:40 +0500

    added forgot password functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :full_name, presence: true
   validates :password, length:{minimum: 8}, format:{with: /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/}
-  validates :full_name, presence: true, uniqueness: { case_sensitive: false }
+  validates :full_name, presence: true
   # Include default devise modules. Others available are:
   # :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -13,12 +13,24 @@ class User < ApplicationRecord
     @login || self.full_name || self.email
   end
 
-  def self.find_for_database_authentication(warden_conditions)
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions.to_hash).where(["lower(full_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  #   elsif conditions.has_key?(:full_name) || conditions.has_key?(:email)
+  #     where(conditions.to_hash).first
+  #   end
+  # end
+  def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_hash).where(["lower(full_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:full_name) || conditions.has_key?(:email)
-      where(conditions.to_hash).first
+      where(conditions).where(["lower(full_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:login].nil?
+        where(conditions).first
+      else
+       where(login: conditions[:login]).first
+      end
     end
   end
 end
