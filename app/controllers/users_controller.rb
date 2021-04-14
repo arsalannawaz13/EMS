2342d2d536d11a4053013db8420966fa352b5d50
Apple <apple@Apples-MacBook-Pro.local>
2021-04-14 13:11:15 +0500

    fixed my profile and added bootstrap navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
-
   before_action :authenticate_user!
+
+  def index;end
+
+  def show
+    @user = User.find(params[:id])
+    render "user/show"
+  end
 
   def edit
     @user = current_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :full_name, presence: true
   validates :password, length:{minimum: 8}, format:{with: /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/}
   validates :full_name, presence: true
+  
   # Include default devise modules. Others available are:
   # :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
