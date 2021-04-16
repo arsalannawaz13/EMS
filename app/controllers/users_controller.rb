44ce7d4ade9fa5884d1aeb87573fefdfa7577bfc
Apple <apple@Apples-MacBook-Pro.local>
2021-04-16 10:47:44 +0500

    PR feedback#2 incorporated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
-
   def show
     render "user/show"
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :full_name, presence: true
-  validates :password, length: {minimum: 8}, format:{with: /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/}
+  validates :password, length: {minimum: 8}, format: {with: /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/}
   validates :full_name, presence: true
   
   # Include default devise modules. Others available are:
