bd183b695dc4035828ae02caa2805aaa36d13d34
Apple <apple@Apples-MacBook-Pro.local>
2021-04-26 11:44:19 +0500

    Added admin invites user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   validates :full_name, presence: true
   validates :password, format: {with: /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/}
-  
   # Include default devise modules. Others available are:
   # :timeoutable, and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :lockable, :confirmable, :invitable
 
   attr_writer :login
